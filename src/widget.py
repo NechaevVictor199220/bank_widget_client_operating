d5f309335af0888c476eebde5878a45c681c68e1
Victor <Nechaevvictor199220@gmail.com>
2025-09-07 17:24:52 +0700

    fix imports

--- src/widget.py
+++ src/widget.py
@@ -1,29 +1,27 @@
-from masks import get_mask_account, get_mask_card_number
+from .masks import get_mask_account, get_mask_card_number
+from datetime import datetime
 
 
 def mask_account_card(account_info: str) -> str:
-         """
-     Функция принимает строку, содержащую тип и номер карты или счета.
-     Возвращает строку с замаскированным номером
-     """
-         parts = account_info.split()
-         if "счет" in account_info.lower() or "account" in account_info.lower():
-             account_number = parts[-1]
-             masked_number = get_mask_account(account_number)
-             return " ".join(parts[:-1] + [masked_number])
-         else:
-             card_number = parts[-1]
-             masked_number = get_mask_card_number(card_number)
-             return " ".join(parts[:-1] + [masked_number])
-
-
-from datetime import datetime
+    """
+    Функция принимает строку, содержащую тип и номер карты или счета.
+    Возвращает строку с замаскированным номером
+    """
+    parts = account_info.split()
+    if "счет" in account_info.lower() or "account" in account_info.lower():
+        account_number = parts[-1]
+        masked_number = get_mask_account(account_number)
+        return " ".join(parts[:-1] + [masked_number])
+    else:
+        card_number = parts[-1]
+        masked_number = get_mask_card_number(card_number)
+        return " ".join(parts[:-1] + [masked_number])
 
 
 def get_date(date_str: str) -> str:
     """
-    Функция принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
- и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024").
+       Функция принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024").
     """
     try:
         date_obj = datetime.fromisoformat(date_str)
