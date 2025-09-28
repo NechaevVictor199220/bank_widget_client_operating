7c39a92ed0d704b4f1ad596d049038a58526a443
Victor <Nechaevvictor199220@gmail.com>
2025-09-28 15:19:22 +0700

    html pytest

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Any, Iterator, Generator
+from typing import Any, Dict, Generator, Iterator, List
 
 
 def filter_by_currency(transactions: List[Dict[str, Any]], currency_code: str) -> Iterator[Dict[str, Any]]:
